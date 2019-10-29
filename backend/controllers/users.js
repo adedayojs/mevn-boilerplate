a587c9e32b7385bf6836a8c62197f6175b2f4e40
adedayojs <samfeolu@gmail.com>
2019-10-29 01:51:12 +0100

    Addition: Controller for user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,8 +1,11 @@
 const Users = require('../models/users');
 
+/* Get User by their id */
 function getUserById(id) {
   return Users.findById(id);
 }
+
+/* Get list of entire users from the database */
 function getAllUsers() {
   return Users.find({});
 }
